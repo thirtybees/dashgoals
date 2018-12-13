6e866b039bed5582f09cfe1cfef589b82c60d8a1
Markus Hitter <mah@jump-ing.de>
2018-12-13 14:44:28 +0100

    Make release 2.0.6.

--- dashgoals.php
+++ dashgoals.php
@@ -46,7 +46,7 @@ class DashGoals extends Module
     {
         $this->name = 'dashgoals';
         $this->tab = 'dashboard';
-        $this->version = '2.0.5';
+        $this->version = '2.0.6';
         $this->author = 'thirty bees';
         $this->need_instance = 0;
 
